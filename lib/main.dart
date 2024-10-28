5a57c549955e03bd5a93b14f8e27a9a909f67abd
Musabumair098 <muhammadmusabumair0777@gmail.com>
2024-10-28 22:04:04 +0500

    this my personal project of insta

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/screens/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Instagram Clone',
+        theme: ThemeData(
+          splashColor: Colors.transparent,
+        ),
+        home: const MainScreen());
+  }
+}
